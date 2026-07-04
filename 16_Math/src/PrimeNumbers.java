9944ee89f35e3c0ca0cd13305172c1567b0312e2
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-04 20:07:48 +0530

    prime numbers

---
+++ 16_Math/src/PrimeNumbers.java
@@ -0,0 +1,14 @@
+public class PrimeNumbers {
+    static void main() {
+        System.out.println(checkPrime(13));
+        System.out.println(checkPrime(93));
+
+    }
+
+    static boolean checkPrime(int n){
+        for(int i = 2; i <= n/2; i++){
+            if(n%i == 0)return false;
+        }
+        return true;
+    }
+}
