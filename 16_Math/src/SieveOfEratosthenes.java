291e7764764aa81ee3c91e086beabe4ede2a74cf
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-06 15:04:05 +0530

    seive of eratosthenes

---
+++ 16_Math/src/SieveOfEratosthenes.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+public class SieveOfEratosthenes {
+    static void main() {
+
+        int b = 100;
+        boolean [] arr = new boolean[b+1];
+
+        sieve(b,arr);
+
+        for (int i = 2; i < b; i++) {
+            if(!arr[i])System.out.print(i + " " );
+        }
+    }
+
+//    space complexity : O(n)
+//    time complexity :
+    static void sieve(int b ,boolean [] arr){
+        int c = 2;
+        while(c*c <= b){
+            if(!arr[c])for (int i = 2; i <= b/c; i++) {
+                arr[i*c] = true;
+            }
+            c++;
+        }
+    }
+
+}
