c732f9ac9f001eec7375ac3bec04c9f992453e56
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-06 14:21:20 +0530

    prime numbers through square root method

--- 16_Math/src/PrimeNumbers.java
+++ 16_Math/src/PrimeNumbers.java
@@ -1,13 +1,23 @@
 public class PrimeNumbers {
     static void main() {
-        System.out.println(checkPrime(13));
-        System.out.println(checkPrime(93));
-        System.out.println(checkPrime(73));
+        System.out.println(isPrime(13));
+        System.out.println(isPrime(1));
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(4));
 
     }
 
     static boolean checkPrime(int n){
         for(int i = 2; i <= n/2; i++){
+            if(n%i == 0)return false;
+        }
+        return true;
+    }
+
+    static boolean isPrime(int n){
+        if(n <= 1)return false;
+        int root = (int)Math.sqrt(n);
+        for(int i = 2; i <= root; i++){
             if(n%i == 0)return false;
         }
         return true;
