c753cf4207fe66ef3ee13ad9607db5bb697d2871
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-07 14:33:56 +0530

    print numbers in ascending , descending or in both order

--- 13_Recursion/src/easy/Nto1.java
+++ 13_Recursion/src/easy/Nto1.java
@@ -3,7 +3,42 @@ package easy;
 public class Nto1 {
     static void main() {
 
+        print_num_desc(5);
+
+        print_num_asc(5);
+        System.out.println();
+
+        print_num_both(5);
+        System.out.println();
+
     }
 
+    static void print_num_desc(int n){
+        if(n == 1){
+            System.out.println(1);
+            return;
+        }
+        System.out.print(n + " ");
+//        print_num_desc(n--); //infinite loop
+        print_num_desc(--n);  // == print_num_desc(n-1)
+    }
+
+    static void print_num_asc(int n){
+        if(n == 0) {
+            return;
+        }
+        print_num_asc(n-1);
+        System.out.print(n + " ");
+    }
+
+    static void print_num_both(int n){
+        if(n == 0){
+            System.out.print(" - rev - ");
+            return;
+        }
+        System.out.print(n + " ");
+        print_num_both(n-1);
+        System.out.print(n + " ");
+    }
 
 }
