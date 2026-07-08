655e30cf65a4a4bc2f0868d971b0fd06ffb5bae6
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-08 11:04:20 +0530

    sum of n to 1

--- 13_Recursion/src/easy/Sum.java
+++ 13_Recursion/src/easy/Sum.java
@@ -1,4 +1,15 @@
 package easy;
 
 public class Sum {
+
+    static void main() {
+
+        System.out.println(sum(10));
+    }
+
+    static int sum(int n){
+        if(n == 0)return 0;
+        return n+sum(n-1);
+    }
+
 }
