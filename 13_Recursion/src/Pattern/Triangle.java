928cf535b18a1e068ce7edd181ea70efd2461b6b
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-11 12:54:02 +0530

    printing triangles by recursive functions

---
+++ 13_Recursion/src/Pattern/Triangle.java
@@ -0,0 +1,37 @@
+package Pattern;
+
+public class Triangle {
+
+    static void main() {
+        triangle1(4);
+        triangle2(4,0);
+    }
+
+    static void triangle1(int n) {
+        if (n == 0) return;
+        cols1(n);
+        triangle1(n - 1);
+    }
+
+    private static void cols1(int n) {
+        if (n == 1) {
+            System.out.println("* ");
+            return;
+        }
+        System.out.print("* ");
+        cols1(n - 1);
+    }
+
+    static void triangle2(int r, int c) {
+        if(r == 0)return;
+        if(c < r){
+            triangle2(r,c+1);
+            System.out.print("* ");
+        }
+        else{
+            triangle2(r-1,0);
+            System.out.println();
+        }
+    }
+
+}
