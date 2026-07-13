c222ba11c7d83ce5160898d61cdf52e66513cf01
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-13 17:02:38 +0530

    bubble sort

--- 13_Recursion/src/Pattern/BubbleSort.java
+++ 13_Recursion/src/Pattern/BubbleSort.java
@@ -1,18 +1,26 @@
 package Pattern;
 
+import java.util.Arrays;
+
 public class BubbleSort {
     static void main() {
+        int[]arr = {5,4,3,2,1};
+        bubbleSort(arr,arr.length,0);
+        System.out.println(Arrays.toString(arr));
     }
 
-    static void bubbleSort(int r, int c) {
+    static void bubbleSort(int[]arr, int r, int c) {
         if(r == 0)return;
-        if(c < r){
-            bubbleSort(r,c+1);
-            System.out.print("* ");
+        if(c+1 < r){
+            if(arr[c] > arr[c+1]){
+                int temp = arr[c];
+                arr[c] = arr[c+1];
+                arr[c+1] = temp;
+            }
+            bubbleSort(arr, r,c+1);
         }
         else{
-            bubbleSort(r-1,0);
-            System.out.println();
+            bubbleSort(arr,r-1,0);
         }
     }
 }
