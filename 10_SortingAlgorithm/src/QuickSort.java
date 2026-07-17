365b73b6a95140a8f383c593f6f09ffeeb3ef16b
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-17 18:25:10 +0530

    quick sort

---
+++ 10_SortingAlgorithm/src/QuickSort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public class QuickSort {
+    static void main() {
+
+        int [] arr = {6,5,4,3,2,1};
+        quicksort(arr,0,arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    static void quicksort(int [] arr, int low , int high){
+        if(low >= high)return ;
+        int s = low, e = high;
+        int m = s + (e-s)/2;
+        int pivot = arr[m];
+
+        while(s <= e){
+//            also a reason why if it's already sorted it will not swap
+            while(arr[s] < pivot)s++;
+            while(arr[e] > pivot) e--;
+
+            if(s <= e){
+                int temp = arr[s];
+                arr[s] = arr[e];
+                arr[e] = temp;
+                s++;
+                e--;
+            }
+        }
+
+//        now my pivot is at correct index , sort the other halves
+        quicksort(arr,low , e);
+        quicksort(arr,s,high);
+    }
+}
