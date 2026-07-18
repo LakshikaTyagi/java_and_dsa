a89ec55d70a2baa43383db91b50afe56343c0775
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-18 16:06:29 +0530

    skip a character

---
+++ 13_Recursion/src/Strings/Stream.java
@@ -0,0 +1,16 @@
+package Strings;
+
+public class Stream {
+    static void main() {
+
+        String s = "baccad";
+        System.out.println(skip("abaccdaaaaf"));
+    }
+
+    static String skip(String str){
+        if (str.isEmpty()) return "";
+        String ch = String.valueOf(str.charAt(0));
+        if(ch.equals("a"))return skip(str.substring(1));
+        return ch + skip(str.substring(1));
+    }
+}
