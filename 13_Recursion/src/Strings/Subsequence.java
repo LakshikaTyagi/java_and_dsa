1308f4deacd97d884882529a7d215051ee2e477e
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-20 15:31:22 +0530

    subsequence(subsets of a string)

---
+++ 13_Recursion/src/Strings/Subsequence.java
@@ -0,0 +1,19 @@
+package Strings;
+
+public class Subsequence {
+    static void main() {
+
+        subseq("","abc");
+    }
+
+    static void subseq(String p , String up){
+        if(up.isEmpty()){
+            System.out.print(p + " ");
+            return;
+        }
+
+        char ch = up.charAt(0);
+        subseq(p + ch , up.substring(1));
+        subseq(p, up.substring(1));
+    }
+}
