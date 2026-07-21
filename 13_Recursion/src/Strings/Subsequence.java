ffc1d6c42608094eb33b45b8758e2e9995d47839
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-21 17:30:30 +0530

    returning an arraylist

--- 13_Recursion/src/Strings/Subsequence.java
+++ 13_Recursion/src/Strings/Subsequence.java
@@ -1,9 +1,16 @@
 package Strings;
 
+import java.util.ArrayList;
+
 public class Subsequence {
     static void main() {
 
         subseq("","abc");
+        System.out.println();
+
+        ArrayList <String> list = new ArrayList<>();
+        list = subseqReturn("","abc");
+        System.out.println(list);
     }
 
     static void subseq(String p , String up){
@@ -15,5 +22,22 @@ public class Subsequence {
         char ch = up.charAt(0);
         subseq(p + ch , up.substring(1));
         subseq(p, up.substring(1));
+    }
+
+    static ArrayList<String> subseqReturn(String p, String up){
+        ArrayList <String> list = new ArrayList<>();
+        if(up.isEmpty()){
+            list.add(p);
+            return list;
+        }
+
+        char ch = up.charAt(0);
+        ArrayList<String> left = subseqReturn(p + ch , up.substring(1));
+        ArrayList <String> right = subseqReturn(p, up.substring(1));
+
+        list.addAll(left);
+        list.addAll(right);
+
+        return list;
     }
 }
