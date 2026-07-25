0d4adb945fde8c75ab036347799ba5cb446fcb6a
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-07-25 12:34:57 +0530

    backtracking problems

--- 13_Recursion/src/Backtracking/AllPaths.java
+++ 13_Recursion/src/Backtracking/AllPaths.java
@@ -1,5 +1,7 @@
 package Backtracking;
 
+import java.util.Arrays;
+
 public class AllPaths {
     static void main() {
         boolean[][] maze = {
@@ -8,8 +10,12 @@ public class AllPaths {
                 {true,true,true}
         };
 
-//        allPathOverflow("",maze,0,0);  //StackOverflowError 1.07
+//        allPathOverflow("",maze,0,0);  //StackOverflowError
         allPaths("",maze,0,0);
+        System.out.printf("\n\n");
+
+        int[][] arr = new int[3][3];
+        allPathPrint("",arr,1,maze,0,0);
 
         
     }
@@ -45,26 +51,62 @@ public class AllPaths {
         if(!maze[r][c]){
             return ;
         }
-        if(r > 0){
-            maze[r][c] = false;
-            allPaths(p + "U",maze,r-1,c);
-            maze[r][c] = true;
-        }
-        if(c > 0){
-            maze[r][c] = false;
-            allPaths(p + "L",maze,r,c-1);
-            maze[r][c] = true;
-        }
+
+//        I am considering it false for this call so that it will not be visited again
+        maze[r][c] = false;
+
         if(r < maze.length - 1){
-            maze[r][c] = false;
             allPaths(p + "D",maze, r+1,c);
-            maze[r][c] = true;
         }
         if(c < maze[0].length - 1){
-            maze[r][c] = false;
             allPaths(p + "R",maze,r,c+1);
-            maze[r][c] = true;
         }
+        if(r > 0){
+            allPaths(p + "U",maze,r-1,c);
+        }
+        if(c > 0){
+            allPaths(p + "L",maze,r,c-1);
+        }
+
+//        this line is where the function will be over
+//        so before the function gets removed, also remove the changes that were made by that function
+        maze[r][c] = true;
     }
+
+    static void allPathPrint(String p, int[][] arr, int level, boolean[][] maze, int r, int c){
+        if(r == maze.length -1  && c == maze[0].length -1 ){
+            arr[r][c] = level;
+            System.out.println(Arrays.deepToString(arr));
+            System.out.println(p);
+            arr[r][c] = 0;
+            return;
+        }
+        if(!maze[r][c]){
+            return ;
+        }
+
+//        I am considering it false for this call so that it will not be visited again
+        maze[r][c] = false;
+        arr[r][c] = level;
+
+        if(r < maze.length - 1){
+            allPathPrint(p + "D",arr,level+1,maze, r+1,c);
+        }
+        if(c < maze[0].length - 1){
+            allPathPrint(p + "R",arr,level+1,maze,r,c+1);
+        }
+        if(r > 0){
+            allPathPrint(p + "U",arr,level+1,maze,r-1,c);
+        }
+        if(c > 0){
+            allPathPrint(p + "L",arr,level+1,maze,r,c-1);
+        }
+
+//        this line is where the function will be over
+//        so before the function gets removed, also remove the changes that were made by that function
+        maze[r][c] = true;
+        arr[r][c] = 0;
+    }
+
 
 }
