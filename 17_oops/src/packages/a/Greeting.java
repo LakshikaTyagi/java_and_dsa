160171a5635fc985768f102882c520f3d237ebac
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-08-02 12:40:48 +0530

    import statement

--- 17_oops/src/packages/a/Greeting.java
+++ 17_oops/src/packages/a/Greeting.java
@@ -1,7 +1,10 @@
 package packages.a;
 
+import static packages.b.Message.message;
+
 public class Greeting {
     static void main() {
-        System.out.println("Hello world");  //11.35
+        System.out.println("Hello world");
+        message();
     }
 }
---
+++ 17_oops/src/packages/b/Message.java
@@ -0,0 +1,11 @@
+package packages.b;
+
+public class Message {
+    static void main() {
+
+    }
+
+    public static void message() {
+        System.out.println("This course is awesome !");
+    }
+}
