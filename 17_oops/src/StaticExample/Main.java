afdda3018d70528f26b944ce1a76478b396efb84
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-08-07 20:55:55 +0530

    static inside non static

--- 17_oops/src/StaticExample/Main.java
+++ 17_oops/src/StaticExample/Main.java
@@ -10,12 +10,31 @@ public class Main {
 
         Human h1 = new Human(29, "abc", 87312, true);
 
-        System.out.println(h.name);
-
 //        it is generally a good practice to access static variables through class names not object names
         System.out.println(Human.population);
-
         System.out.println(h1.population);
 
+//        Main funn = new Main();
+//        funn.fun2();
+
+    }
+
+    //    this is not dependent on objects
+    static void fun() {
+//        greeting(); //you cannot use this because it requires an instance
+//        but the function you are using it in does not depend on instances
+
+//        you cannot access non - static stuff without referencing their instances in static context
+        Main obj = new Main();
+        obj.greeting();
+    }
+
+    void fun2() {
+        greeting();
+    }
+
+    public void greeting() {
+        fun();
+        System.out.println("Hello world !");
     }
 }
