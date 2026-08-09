aee442c1554e40b7c5eae8c4fa670196ec02f95f
lakshika tyagi <lakshikatyagi02@gmail.com>
2026-08-09 12:39:28 +0530

    non static inside static

--- 17_oops/src/StaticExample/Human.java
+++ 17_oops/src/StaticExample/Human.java
@@ -7,6 +7,11 @@ public class Human {
     boolean married;
     static long population;
 
+    static void message() {
+        System.out.println("Hello world");
+//        System.out.println(this.age);  //cannot reference it
+    }
+
     public Human(int age, String name, int salary, boolean married) {
         this.age = age;
         this.name = name;
@@ -14,5 +19,10 @@ public class Human {
         this.married = married;
 //        this.population += 1;  //same output while using Human
         Human.population += 1;
+
+        Human.message();
+
     }
+
+
 }
